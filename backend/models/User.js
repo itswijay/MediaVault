80d96238792d838ce087d646c18ba7a622ca2490
Pubudu Wijesundara <neo17641@gmail.com>
2026-01-03 19:26:32 +0530

    fix(user): simplify password hashing middleware by removing next() callback

--- backend/models/User.js
+++ backend/models/User.js
@@ -50,18 +50,17 @@ const userSchema = new mongoose.Schema(
 )
 
 // Hash password before saving
-userSchema.pre('save', async function (next) {
+userSchema.pre('save', async function () {
   // Only hash if password is modified or new
   if (!this.isModified('password')) {
-    return next()
+    return
   }
 
   try {
     const salt = await bcrypt.genSalt(10)
     this.password = await bcrypt.hash(this.password, salt)
-    next()
   } catch (error) {
-    next(error)
+    throw error
   }
 })
 
