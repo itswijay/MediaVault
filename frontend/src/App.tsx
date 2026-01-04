d2788e81b7c2c1162acdd8b7bbf7c1221512a8c6
Pubudu Wijesundara <neo17641@gmail.com>
2026-01-04 12:40:02 +0530

    Initialize the react frontend with tailwind and shadcnUI

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='text-fuchsia-500 font-extralight mb-4'>Hello World</h1>
+        <Button>Test</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
